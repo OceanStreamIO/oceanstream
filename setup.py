dfa7bf19fa4d99c2db3cb5ed3362c15586c3339d
Mihai Boldeanu <35964395+mihaiboldeanu@users.noreply.github.com>
2023-10-04 11:52:46 +0300

    Updated gitignore to not track .vs folder for Visual studio code projects and updated setup.py folder to correctly install from requirements.txt (#71)

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages, setup
 
+# Reading requirements from 'requirements.txt'
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f.readlines()]
+
 setup(
-    name="your_package_name",
-    version="0.1",
+    name="oceanstream",
+    version="0.1",  # You can adjust the version number as needed
     packages=find_packages(),
-    install_requires=[
-        # List your project dependencies here
-    ],
+    install_requires=requirements,
+    # Optional metadata
+    author="PineView AS",
+    author_email="hello@pineview.io",
+    description="A brief description of the oceanstream package",
+    license="MIT",
+    keywords="ocean stream processing",
+    url="https://github.com/OceanStreamIO/oceanstream",
 )
