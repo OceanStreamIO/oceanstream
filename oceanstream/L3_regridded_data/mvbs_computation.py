3b7723613e62c132bf69c24c1d5cb040da63e521
Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
2023-10-26 15:10:18 +0300

    Add MVBS
    Compute Mean Volume Backscattering Strength (MVBS)

---
+++ oceanstream/L3_regridded_data/mvbs_computation.py
@@ -0,0 +1,77 @@
+from typing import Literal, Optional
+
+import echopype as ep
+import xarray as xr
+
+
+def compute_mvbs(
+    ds_Sv: xr.Dataset,
+    method: Literal["index_binning", "physical_units"] = "physical_units",
+    range_sample_num: Optional[int] = None,
+    ping_num: Optional[int] = None,
+    range_var: Optional[Literal["echo_range", "depth"]] = None,
+    range_bin: Optional[str] = None,
+    ping_time_bin: Optional[str] = None,
+) -> xr.Dataset:
+    """
+    Wrapper function around echopype's `echopype.commongrid.api.compute_MVBS`
+    and `echopype.commongrid.api.compute_MVBS_index_binning`.
+    - `echopype.commongrid.api.compute_MVBS`:
+        Compute Mean Volume Backscattering Strength (MVBS)
+        based on intervals of range (`echo_range`) or depth (`depth`)
+        and `ping_time` specified in physical units.
+    - `echopype.commongrid.api.compute_MVBS_index_binning`:
+        Compute Mean Volume Backscattering Strength (MVBS)
+        based on intervals of range sample number(`range_sample_num`)
+        and ping number (`ping_num`) specified in index number.
+
+    Parameters:
+
+    - ds_Sv : xr.Dataset
+        Input dataset containing Sv
+    - method : str
+        Choose method type. Either "index_binning" or "physical_units"
+    - range_sample_num, ping_num
+        Parameters for "index_binning" method. Defaults are 100.
+    - range_var: {'echo_range', 'depth'}, default 'echo_range'
+        Parameter for "physical_units" method.
+        The variable to use for range binning.
+        Must be one of `echo_range` or `depth`.
+        Note that `depth` is only available if the input dataset contains
+        `depth` as a data variable.
+    - range_bin, ping_time_bin
+        Parameters for "physical_units" method. Defaults are "20m", and "20S" respectively.
+
+    Returns:
+
+    - A dataset containing bin-averaged Sv.
+    """
+
+    methods = {
+        "index_binning": ep.commongrid.api.compute_MVBS_index_binning,
+        "physical_units": ep.commongrid.api.compute_MVBS,
+    }
+
+    if method == "index_binning":
+        if any([range_var, range_bin, ping_time_bin]):
+            raise ValueError(
+                "For 'index_binning' method, only range_sample_num and ping_num should be provided."
+            )
+        range_sample_num = range_sample_num if range_sample_num is not None else 100
+        ping_num = ping_num if ping_num is not None else 100
+        return methods[method](ds_Sv, range_sample_num, ping_num)
+
+    elif method == "physical_units":
+        if any([range_sample_num, ping_num]):
+            raise ValueError(
+                "For 'physical_units' method, only range_var, range_bin, and ping_time_bin should be provided."
+            )
+        range_var = range_var if range_var is not None else "echo_range"
+        range_bin = range_bin if range_bin is not None else "20m"
+        ping_time_bin = ping_time_bin if ping_time_bin is not None else "20S"
+        return methods[method](
+            ds_Sv, range_var=range_var, range_bin=range_bin, ping_time_bin=ping_time_bin
+        )
+
+    else:
+        raise ValueError("Invalid method. Choose either 'index_binning' or 'physical_units'.")
---
+++ tests/test_mvbs_computation.py
@@ -0,0 +1,115 @@
+import echopype as ep
+import numpy as np
+import pandas as pd
+import pytest
+import xarray as xr
+
+from oceanstream.L2_calibrated_data.sv_computation import compute_sv
+from oceanstream.L2_calibrated_data.sv_dataset_extension import enrich_sv_dataset
+from oceanstream.L3_regridded_data.mvbs_computation import compute_mvbs
+
+
+def generate_mock_data(channels=2, pings=4, ranges=4):
+    """
+    Generate a mock dataset with given dimensions.
+
+    Parameters:
+    - channels (int): Number of channels.
+    - pings (int): Number of ping times.
+    - ranges (int): Number of range samples.
+
+    Returns:
+    - Mock dataset (xarray.DataArray).
+    """
+    # Data
+    data = np.array(
+        [
+            [[-80, 0, 0, 0], [-80, 0, 0, 0], [-80, 0, 0, 0], [-80, 0, 0, 0]],  # Channel 1
+            [[-1, 0, 0, 0], [-1, 0, 0, 0], [-1, 0, 0, 0], [-1, 0, 0, 0]],  # Channel 2
+        ]
+    )
+
+    # Channel names
+    channel_names = ["Channel " + str(i + 1) for i in range(channels)]
+
+    # Create datetime range for ping times
+    ping_times = pd.date_range("2017-06-20T01:10:27", periods=pings, freq="S")
+
+    # Range sample values
+    range_sample = np.arange(ranges)
+
+    # Create the xarray DataArray
+    sv = xr.DataArray(
+        data=data,
+        coords={"channel": channel_names, "ping_time": ping_times, "range_sample": range_sample},
+        dims=["channel", "ping_time", "range_sample"],
+    )
+
+    return sv
+
+
+def test_index_binning_wrong_params():
+    ds = generate_mock_data(2, 4, 4)
+    with pytest.raises(
+        ValueError,
+        match="For 'index_binning' method, only range_sample_num and ping_num should be provided.",
+    ):
+        compute_mvbs(ds, method="index_binning", range_var="depth")
+    with pytest.raises(
+        ValueError,
+        match="For 'index_binning' method, only range_sample_num and ping_num should be provided.",
+    ):
+        compute_mvbs(ds, method="index_binning", range_bin="30m")
+    with pytest.raises(
+        ValueError,
+        match="For 'index_binning' method, only range_sample_num and ping_num should be provided.",
+    ):
+        compute_mvbs(ds, method="index_binning", ping_time_bin="30S")
+
+
+def test_physical_units_wrong_params():
+    ds = generate_mock_data(2, 4, 4)
+    with pytest.raises(
+        ValueError,
+        match="For 'physical_units' method, only range_var, range_bin, and ping_time_bin should be provided.",
+    ):
+        compute_mvbs(ds, method="physical_units", range_sample_num=150)
+    with pytest.raises(
+        ValueError,
+        match="For 'physical_units' method, only range_var, range_bin, and ping_time_bin should be provided.",
+    ):
+        compute_mvbs(ds, method="physical_units", ping_num=150)
+
+
+def test_compute_mvbs(ed_ek_60_for_Sv):
+    sv_echopype_EK60 = compute_sv(ed_ek_60_for_Sv)
+    enriched_sv = enrich_sv_dataset(
+        sv_echopype_EK60, ed_ek_60_for_Sv, depth_offset=200, waveform_mode="CW", encode_mode="power"
+    )
+
+    ds_ep = ep.commongrid.api.compute_MVBS_index_binning(enriched_sv)
+    ds_os = compute_mvbs(enriched_sv, method="index_binning")
+    result = np.isclose(ds_ep["Sv"].values, ds_os["Sv"].values, atol=1e-8, equal_nan=True)
+    assert result.all()
+    ds_ep = ep.commongrid.api.compute_MVBS(enriched_sv)
+    ds_os = compute_mvbs(enriched_sv, method="physical_units")
+    result = np.isclose(ds_ep["Sv"].values, ds_os["Sv"].values, atol=1e-8, equal_nan=True)
+    assert result.all()
+    ds_ep = ep.commongrid.api.compute_MVBS_index_binning(
+        enriched_sv, range_sample_num=30, ping_num=30
+    )
+    ds_os = compute_mvbs(enriched_sv, method="index_binning", range_sample_num=30, ping_num=30)
+    result = np.isclose(ds_ep["Sv"].values, ds_os["Sv"].values, atol=1e-8, equal_nan=True)
+    assert result.all()
+    ds_ep = ep.commongrid.api.compute_MVBS(
+        enriched_sv, range_var="depth", range_bin="90m", ping_time_bin="60S"
+    )
+    ds_os = compute_mvbs(
+        enriched_sv,
+        method="physical_units",
+        range_var="depth",
+        range_bin="90m",
+        ping_time_bin="60S",
+    )
+    result = np.isclose(ds_ep["Sv"].values, ds_os["Sv"].values, atol=1e-8, equal_nan=True)
+    assert result.all()
