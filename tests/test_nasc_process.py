cfa84652f728ddd27e6e61d25e8c6f34ef5a5ff9
Mihai Boldeanu <35964395+mihaiboldeanu@users.noreply.github.com>
2023-11-14 16:11:06 +0200

    Updated default params for transient noise mask creation (#134)

--- tests/test_nasc_process.py
+++ tests/test_nasc_process.py
@@ -16,11 +16,11 @@ def test_compute_per_dataset_nasc(ek_60_Sv_full_denoised):
 def test_compute_masked_nasc(ek_60_Sv_shoal):
     dataset = ek_60_Sv_shoal
     fish_nasc = mask_nasc_data(dataset, {"mask_shoal": False}, True, "fish_NASC")
-    assert fish_nasc['fish_NASC_38000.0'] == pytest.approx(131203167, 0.0001)
+    assert fish_nasc['fish_NASC_38000.0'] == pytest.approx(130821359, 0.0001)
 
 @pytest.mark.ignore
 def test_compute_full_nasc(ek_60_Sv_shoal):
     dataset = ek_60_Sv_shoal
     nasc = full_nasc_data(dataset, BASE_NASC_PARAMETERS)
     assert len(nasc) == 24
-    assert nasc['fish_NASC_38000.0'] == pytest.approx(131203167, 0.0001)
+    assert nasc['fish_NASC_38000.0'] == pytest.approx(130821359, 0.0001)
