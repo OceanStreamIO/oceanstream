3f8ceacd19bbb40554f14b70e2240fa20ae7bc45
Andrei Rusu <andrei.rusu@beatfactor.net>
2023-11-27 09:44:43 +0100

    Moved test data from main module to tests

--- tests/test_noise_mask.py
+++ tests/test_noise_mask.py
@@ -11,6 +11,59 @@ from oceanstream.L2_calibrated_data.noise_masks import (
     create_multiple_masks,
 )
 
+TEST_MASK_PARAMETERS = {
+    "transient": {
+        "method": "ryan",
+        "params": {
+            "m": 5,
+            "n": 5,
+            "thr": 20,
+            "excludeabove": 250,
+            "dask_chunking": {"ping_time": 100},
+            "operation": "mean",
+        },
+    },
+    "attenuation": {
+        "method": "ryan",
+        "params": {
+            "r0": 180,
+            "r1": 280,
+            "n": 5,
+            "m": None,
+            "thr": -5,
+            "start": 0,
+            "offset": 0,
+        },
+    },
+    "impulse": {"method": "ryan", "params": {"thr": 3, "m": 3, "n": 1}},
+    "seabed": {
+        "method": "ariza",
+        "params": {
+            "r0": 10,
+            "r1": 1000,
+            "roff": 0,
+            "thr": -40,
+            "ec": 1,
+            "ek": (1, 3),
+            "dc": 10,
+            "dk": (3, 7),
+        },
+    },
+    "false_seabed": {
+        "method": "blackwell",
+        "params": {
+            "theta": None,
+            "phi": None,
+            "r0": 10,
+            "r1": 1000,
+            "tSv": -75,
+            "ttheta": 702,
+            "tphi": 282,
+            "wtheta": 28,
+            "wphi": 52,
+        },
+    },
+}
 
 def test_impulse(enriched_ek60_Sv):
     source_Sv = enriched_ek60_Sv
@@ -48,7 +101,7 @@ def test_seabed(enriched_ek60_Sv):
 
 def test_create_masks(enriched_ek60_Sv):
     enriched_Sv = enriched_ek60_Sv
-    Sv_mask = create_multiple_masks(enriched_Sv)
+    Sv_mask = create_multiple_masks(enriched_Sv, TEST_MASK_PARAMETERS)
     assert Sv_mask["mask_seabed"].attrs["mask_type"] == "seabed"
     assert Sv_mask["mask_impulse"].attrs["parameters"] == ["thr=3", "m=3", "n=1"]
 
