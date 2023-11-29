3f9957ee27300ccb0631911e3649fcd58caf7277
Mihai Boldeanu <mishu_boldy@yahoo.com>
2023-11-29 10:17:30 +0200

    Added the downsampling feature to same module as interpolation

--- oceanstream/L2_calibrated_data/__init__.py
+++ oceanstream/L2_calibrated_data/__init__.py
@@ -11,5 +11,5 @@ from .noise_masks import (
 from .processed_data_io import read_processed, write_processed
 from .sv_computation import compute_sv
 from .sv_dataset_extension import enrich_sv_dataset
-from .sv_interpolation import interpolate_sv
+from .sv_interpolation import interpolate_sv, regrid_dataset
 from .target_strength_computation import compute_target_strength
